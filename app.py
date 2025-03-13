de03be3ec3dc44f86ae2d4680262acde45dd9e26
Alex Telenkov <ru3aah@gmail.com>
2025-03-13 12:54:33 +0300

    refactor do_GET & do_POST

--- app.py
+++ app.py
@@ -13,40 +13,56 @@ MAX_FILE_SIZE = 1024 * 1024 * 5  # 5 MB
 class ImageHostingHTTPRequestHandler(BaseHTTPRequestHandler):
     server_version = 'Image Hosting Server v0.1'
 
+    def __init__(self, request, client_address, server):
+        self.get_routes = {
+            '/api/images': self.get_images,
+            '/upload': self.get_upload
+        }
+        self.post_routes = {
+            '/upload': self.post_upload,
+        }
+        self.default = lambda : self.send_html('404.html', code=404)
+
+        super().__init__(request, client_address, server)
+
+
     # handles all GET requests
     def do_GET(self):
-        if self.path == '/api/images':
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            response = {
-                'images': next(os.walk(IMAGESTORE_PATH))[2]
-            }
-            self.wfile.write(json.dumps(response).encode('utf-8'))
-        else:
-            self.send_html('404.html', code=404)
+        self.get_routes.get(self.path, self.default)()
 
 
     def do_POST(self):
-        if self.path == '/upload':
-            file_size = int(self.headers.get('Content-Length'))
-            if file_size > MAX_FILE_SIZE:
-                self.send_html('file_too_large.html', code=413)
-                return
+        self.post_routes.get(self.path, self.default)()
 
-            data = self.rfile.read(file_size)
-            _, file_ext = os.path.splitext(self.headers.get('Filename'))
-            image_id = uuid4()
 
-            if file_ext not in ALLOWED_EXTENSIONS:
-                self.send_html('invalid_file_type.html', code=400)
-                return
+    def get_images(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response = {
+            'images': next(os.walk(IMAGESTORE_PATH))[2]
+        }
+        self.wfile.write(json.dumps(response).encode('utf-8'))
 
-            with open(IMAGESTORE_PATH + f'{image_id}{file_ext}', 'wb') as file:
-                file.write(data)
-            self.send_html('upload_success.html')
-        else:
-            self.send_html('404.html', code=404)
+    def get_upload(self):
+        self.send_html('upload.html', code =200)
+
+    def post_upload(self):
+        file_size = int(self.headers.get('Content-Length'))
+        if file_size > MAX_FILE_SIZE:
+            self.send_html('file_too_large.html', code=413)
+            return
+
+        data = self.rfile.read(file_size)
+        _, file_ext = os.path.splitext(self.headers.get('Filename'))
+        image_id = uuid4()
+
+        if file_ext not in ALLOWED_EXTENSIONS:
+            self.send_html('invalid_file_type.html', code=400)
+            return
+        with open(IMAGESTORE_PATH + f'{image_id}{file_ext}', 'wb') as file:
+            file.write(data)
+        self.send_html('upload_success.html')
 
 
     def send_html(self, file_path, code=200):
