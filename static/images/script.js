d553f847b8ec0a85a54c78c08752f45502c97774
Alex Telenkov <ru3aah@gmail.com>
2025-03-16 00:37:39 +0300

    updated upload & images page design

---
+++ static/images/script.js
@@ -0,0 +1,31 @@
+const tbody = document.getElementById('imagesTableBody');
+fetch('/api/images').then(response => response.json()).then(images => setImages(images.images));
+
+function setImages(images) {
+    const imagesContainer = document.createElement('div');
+    images.forEach(image => {
+        const tr = document.createElement('tr');
+
+        const tdPreview = document.createElement('td');
+        const tdUrl = document.createElement('td');
+        const tdDelete = document.createElement('td');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-btn');
+        deleteButton.textContent = 'X';
+        tdDelete.appendChild(deleteButton);
+        tdPreview.innerHTML = `<img src="/images/${image}" width="42" height="100%">`;
+        tdUrl.innerHTML = `<a href="/images/${image}" target="_blank">${image}</a>`;
+
+        tr.appendChild(tdPreview);
+        tr.appendChild(tdUrl);
+        tr.appendChild(tdDelete);
+
+        tbody.appendChild(tr);
+    });
+    document.body.appendChild(imagesContainer);
+}
+
+document.getElementById('btnGoToUpload').addEventListener('click', (event) => {
+    window.location.href = '/upload/';
+});
