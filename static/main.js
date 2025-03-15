3f261e742b64564c18d1d56c4e68de1fdfc55dee
Alex Telenkov <ru3aah@gmail.com>
2025-03-15 20:51:41 +0300

    update main.js

--- static/main.js
+++ static/main.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const heroImages = [
-    'background_images/hero1.png',
-    'background_images/hero2.png',
-    'background_images/hero3.png',
-    'background_images/hero4.png'
+    'background_images/1.png',
+    'background_images/2.png',
+    'background_images/3.png',
+    'background_images/4.png',
+    'background_images/5.png'
   ];
 
   const randomIndex = Math.floor(Math.random() * heroImages.length);
